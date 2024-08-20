f7a60b57a5cbd1e8931409179a9dcb56c5eb0234
Archana Kakani <archana.kakani@ibm.com>
2024-08-20 08:11:18 -0500

    Add version to BaseBiosTable to prevent data loss
    The initial version of the Base BIOS table did not support Value Display
    Names (VDN). Support for VDN was introduced in a commit [1]. A BMC image
    created without VDN support stores data in the old BaseBiosTable format.
    If a new BMC image (with VDN support) is flashed over an older image
    (without VDN support), the BIOS manager attempts to read the binary
    archive generated by the older BIOS settings manager. Since it doesn't
    recognize the newly added property, it assumes the persisted file is
    corrupted and ignores all the saved settings.
    
    To prevent the loss of persisted BIOS data, the versioning support
    provided by the Cereal library is used. This version information is
    utilized to convert the persisted data into the current supported
    format.
    
    [1]: https://github.com/openbmc/bios-settings-mgr/commit/1a448ad88fdaec7e082b4a1c437f7f3c990402cd
    
    Change-Id: If68be37e32ae31d7338d8933df945453a30c113a
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

--- src/manager_serialize.cpp
+++ src/manager_serialize.cpp
@@ -10,7 +10,11 @@
 #include <phosphor-logging/lg2.hpp>
 
 #include <fstream>
+#include <map>
+#include <variant>
+#include <vector>
 
+CEREAL_CLASS_VERSION(bios_config::Manager, 1);
 namespace bios_config
 {
 
@@ -34,8 +38,7 @@ static std::uint32_t currentVersion = BIOS_CONFIG_VERSION;
  *                       across code levels
  */
 template <class Archive>
-void save(Archive& archive, const Manager& entry,
-          const std::uint32_t /*version*/)
+void save(Archive& archive, const Manager& entry, const std::uint32_t version)
 {
     std::uint32_t version = BIOS_CONFIG_VERSION;
     archive(version);
@@ -69,8 +72,10 @@ void save(Archive& archive, const Manager& entry,
  *                       across code levels
  */
 template <class Archive>
-void load(Archive& archive, Manager& entry, const std::uint32_t /*version*/)
+void load(Archive& archive, Manager& entry, const std::uint32_t version)
 {
+    lg2::error("Load is called with version {VER}", "VER", version);
+
     Manager::BaseTable baseTable;
     Manager::BaseTableV1 baseTableV1;
 
@@ -100,6 +105,7 @@ void load(Archive& archive, Manager& entry, const std::uint32_t /*version*/)
 
     entry.sdbusplus::xyz::openbmc_project::BIOSConfig::server::Manager::
         baseBIOSTable(baseTable, true);
+
     entry.sdbusplus::xyz::openbmc_project::BIOSConfig::server::Manager::
         pendingAttributes(pendingAttrs, true);
     entry.sdbusplus::xyz::openbmc_project::BIOSConfig::server::Manager::
